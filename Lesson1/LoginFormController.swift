1d02ca210df0fa2ce8e4bd6b6daf26fed86d2a04
Pavel <rig.86@mail.ru>
2021-04-10 11:14:37 +0300

    HW2.2 Did Segues

--- Lesson1/LoginFormController.swift
+++ Lesson1/LoginFormController.swift
@@ -11,20 +11,39 @@ class LoginFormController: UIViewController {
 
    
     @IBOutlet weak var loginTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    let fromFirstPageToSecondSegue = "fromFirstPageToSecondSegue"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    @IBAction func pressSignButton(_ sender: Any) {
+    
+    @IBAction func switchFillLogun(_ sender: Any) {
+        loginTextField.text = "admin"
+        passwordTextField.text = "123456"
+    }
+   
+    
+    @IBAction func pressSignButton(_ sender: UIButton) {
         if self.loginTextField.text == "admin",
            self.passwordTextField.text == "123456" {
-            performSegue(withIdentifier: "pinkSegue", sender: self)
+            performSegue(withIdentifier: fromFirstPageToSecondSegue, sender: self)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Entered wrong login or password", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
         }
     }
     
     
 }
+
